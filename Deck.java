91771d87232a2250b4b4e71b868e3beabc4de51f
IAmSpartans <81576115+IAmSpartans@users.noreply.github.com>
2021-04-26 08:52:00 -0600

    Add files via upload

---
+++ Deck.java
@@ -0,0 +1,19 @@
+/* Deck for BlackJack
+ * Matthew W. Gavin R. Caleb
+ * 4 April 2021
+ * First Create the 4 suits
+ */
+
+import java.util.Scanner;
+
+public class Deck {
+    private Scanner scanner = new Scanner(System.in);
+    private int users;
+    private Player[] players;
+    private Deck deck;
+    private Dealer dealer = new Dealer();
+
+    // public int[] {"Spades", "Diamonds", "Clubs", "Hearts"};
+    // public char[] {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
+}
