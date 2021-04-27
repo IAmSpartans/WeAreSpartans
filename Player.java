b1669232ebb8468402daf36a79891bbd0c582730
IAmSpartans <81576115+IAmSpartans@users.noreply.github.com>
2021-04-27 09:32:49 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,2 +1,25 @@
+/* Printing players for BlackJack
+ * Matthew W. Gavin R. Caleb P. Sean C.
+ * 26 April 2021
+ * Where I keep the information about each player
+ */
+
+
 public class Player {
+    private String name;
+    private int playersCard;
+
+    public Player(String n, int player) {
+        setName(n, player);
+    }
+
+    public Player() {
+
+    }
+
+    public void setName(String n, int player) {
+        name = n;
+        playersCard = player;
+
+    }
 }
