c91392ff6d6602a40dddedee7c76a8d94286edbd
calebjalapanio <56417105+calebjalapanio@users.noreply.github.com>
2021-04-27 18:41:09 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,13 +2,46 @@
  * Matthew W. Gavin R. Caleb P. Sean C.
  * 26 April 2021
  * Where I keep the information about each player
+ * Where the player makes moves
  */
 
+import java.util.InputMismatchException;
+
+import java.util.Scanner;
 
 public class Player {
-    Deck deck = new Deck();
-    int compValue = deck.getCompValue();
-    int playerValue = deck.getPlayerValue();
+//i know this doesn't do exactly what it supposed to but that's on purpose ill fix it.
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Deck deck = new Deck();
+        int compValue = deck.getCompValue();
+        int playerValue = deck.getPlayerValue();
+        //i don't know if these are the correct terms
+        boolean loop = false;
+        while(!loop){
+            try{
+                System.out.println("would you like to hit or pass");
+                String playerChoice = input.nextLine();
+                playerChoice = playerChoice.toUpperCase();
+
+                loop = true;
+                if(playerChoice.equals("HIT")){
+                    playerValue = playerValue + deck.getNewCard();
+                    System.out.println(playerValue);
+                }else if(playerChoice.equals("PASS")){
+
+                }else{
+                    System.out.println("Your input was invalid...");
+                    loop = false;
+                }
+            }catch(InputMismatchException e){
+                System.out.println("please try again:");
+            }
+        }
+
+    }
+
+
     private String name;
     private int playersCard;
 
