630c078eb7b07259bada3ce0cec5e31df5c8ffc2
IAmSpartans <81576115+IAmSpartans@users.noreply.github.com>
2021-05-09 15:48:04 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,49 +1,42 @@
-
 /* Main for BlackJack
  * Matthew W. Gavin R. Caleb P. Sean C.
  * 27 April 2021
  * Controls the flow of the game
  */
 
-
-
 import java.util.Objects;
 import java.util.Scanner;
-
 public class Main {
 
-    String players;
+    Scanner input = new Scanner(System.in);
+    public static String playersone;
     public void playerName(){
         Scanner input = new Scanner(System.in);
         System.out.print("Welcome to BlackJack\n" + "What is the name of the first player? ");
-        players = input.nextLine();
+        playersone = input.next();
     }
 
-
     String choice;
-
     public void getPlayerColor() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("would you like your color to be green or blue");
         String co = scanner.nextLine();
         co = co.toUpperCase();
         if(co.equals("GREEN")){
-            choice = Color.GREEN;
+            choice = Colors.GREEN;
         } else if(co.equals("BLUE")){
-            this.choice = Color.BLUE;
+            this.choice = Colors.BLUE;
         }
     }
-
     public String getCompColor(){
         String def;
-        if(Objects.equals(this.choice, Color.GREEN)){
-            def = Color.BLUE;
+        if(Objects.equals(this.choice, Colors.GREEN)){
+            def = Colors.BLUE;
         }else{
-            def = Color.GREEN;
+            def = Colors.GREEN;
         }
         return def;
     }
-
     void run(){
         Player game = new Player();
         Money money = new Money();
@@ -59,7 +52,6 @@ public class Main {
             if (!choice.equals("YES")) System.out.println("goodbye");
         }while(choice.equals("YES"));
     }
-
     public static void main(String[] args) {
         Main main = new Main();
         main.getPlayerColor();
@@ -67,5 +59,5 @@ public class Main {
         main.playerName();
         main.run();
     }
-
 }
+
