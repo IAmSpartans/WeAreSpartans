82f8f5def493a4feaf5679de015d0de2ef9ccc94
IAmSpartans <81576115+IAmSpartans@users.noreply.github.com>
2021-05-09 15:42:11 -0600

    Update Color.java

--- Color.java
+++ Color.java
@@ -1,4 +1,10 @@
-public class Color {
+/* Colors for BlackJack
+ * Matthew W. Gavin R. Caleb P. Sean C.
+ * 27 April 2021
+ * Colors of the game
+ */
+
+public class Colors {
     // Regular Colors
     public static final String RESET = "\u001B[0m";
     public static final String BLACK = "\u001B[30m";
